553b1b128b83927829be6d69da05806da42a00fb
MikeBLambert <mlamb128@gmail.com>
2018-01-27 20:11:17 -0800

    add javascript to detect whether or not a word is a palindrome

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//Business Logic
+function palindromeTest (a) {
+  var testPhrase = a.split("");
+  for (var i=0; i<testPhrase.length; i+=1) {
+    if (testPhrase[i]!==testPhrase[testPhrase.length-(i+1)]) {
+      return "Not a Palindrome"
+    } else {
+      return "Palindrome!"
+    }
+    ;
+  };
+};
+
+//User Interface
+$(document).ready(function() {
+  $("#palindromeInput").submit(function(event) {
+  event.preventDefault();
+  var wordToTest = $("#wordOrPhrase").val();
+  var palindromeOrNot = palindromeTest(wordToTest);
+  $("#output").html(palindromeOrNot);
+  });
+});
