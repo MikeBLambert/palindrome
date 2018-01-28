d7ffee7298e851ed6e095de9f710aa94ee549bc0
MikeBLambert <mlamb128@gmail.com>
2018-01-27 20:42:38 -0800

    add fix for problem in which function only tests first and last letters. working now

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 //Business Logic
 function palindromeTest (a) {
   var testPhrase = a.split("");
-  for (var i=0; i<testPhrase.length; i+=1) {
+  for (var i=0; i<=testPhrase.length; i+=1) {
     if (testPhrase[i]!==testPhrase[testPhrase.length-(i+1)]) {
-      return "Not a Palindrome"
+      return "Not a Palindrome!";
     } else {
-      return "Palindrome!"
-    }
-    ;
+    };
   };
+  return "Palindrome!"
 };
 
 //User Interface
